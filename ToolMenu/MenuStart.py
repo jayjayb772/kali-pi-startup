1d62720fea3fa2ba873b3d555201c479aca40f39
J Bender <jayjayb100@msn.com>
2020-11-11 15:12:23 -0600

    Update MenuStart.py

--- ToolMenu/MenuStart.py
+++ ToolMenu/MenuStart.py
@@ -1,11 +1,23 @@
-from Tkinter import *
+import tkinter
 
-root = tkinter.TK()
+root = tkinter.Tk()
 root.title("KaliPad Menu")
-
+text = tk.Text(root, width=15, height=3)
 options = ["Wi-Fi attacks", "Local Network", "Utilities"]
 menu_option = tkinter.StringVar()
 menu_option.set(options[0])
 
+def updateOptions():
+    text.insert(tkinter.END, menu_option.get()
+    
+
+
 main_option_menu=tkinter.OptionMenu(root, menu_option, *options)
+menu = tkinter.Menu(root)
+
+main_option_menu.pack()
+root.config(menu=menu)
+
+
+root.mainloop()
 
