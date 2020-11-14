8d7c331294199357c63675e8d2070c60a92bc67a
J Bender <jayjayb100@msn.com>
2020-11-14 13:18:29 -0600

    added other iface back

--- ToolMenu/codeUtils/interfaceClass.py
+++ ToolMenu/codeUtils/interfaceClass.py
@@ -7,8 +7,10 @@ class Interface:
         print("hello world")
         # get ip of iface
         ni.ifaddresses(self.name)
-        print(ni.ifaddresses(self.name))
-        self.ip = ni.ifaddresses(self.name)[ni.AF_INET][0]['addr']
+        try:
+            self.ip = ni.ifaddresses(self.name)[ni.AF_INET][0]['addr']
+        except():
+            print("cannot init " + self.name)
         temp = self.ip.split(".")
         temp[3] = "1-255"
         dot = "."
