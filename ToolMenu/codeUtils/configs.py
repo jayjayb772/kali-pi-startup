f2100d9d63f2fadc488a76a4deb1db6c04eb923e
J Bender <jayjayb100@msn.com>
2020-11-24 14:28:06 -0600

    freqShow Icon\

--- ToolMenu/codeUtils/configs.py
+++ ToolMenu/codeUtils/configs.py
@@ -2,7 +2,7 @@ configs = {
     'terminal_configs': {
         'terminal_start_die_left': "gnome-terminal --geometry 97x11+0+0 -- ",
         'terminal_start_die_right': "",
-        'terminal_start_stay_alive_left': "gnome-terminal --geometry 80x10+0+0 -e 'bash -c \"",
+        'terminal_start_stay_alive_left': "gnome-terminal --geometry 97x11+0+0 -- 'bash -c \"",
         'terminal_start_stay_alive_right': "; bash\" '"
     }
 }
